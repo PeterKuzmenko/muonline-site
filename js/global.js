69e547ffd16c34507eb3c8d589fe4a87dd973e2c
PeterKuzmenko <petr.kuzmenko.04@gmail.com>
2021-08-18 19:14:45 +0300

    muonline-site

---
+++ js/global.js
@@ -0,0 +1,59 @@
+$(".buttonDrop").click(function(){
+  $("."+$(this).attr("data-class")).toggleClass("active");
+  $(this).toggleClass("active");
+});
+
+$('.newsTabs-button').click(function(){
+	$('.newsTabs-button').removeClass('active');
+	$(this).addClass('active');
+	$('.tabsBlock').removeClass('active');
+	$('#'+$(this).attr('data-tab')).addClass('active');
+})
+
+var res = $(".dropDown-menu");
+$('[data-class^="m"]').on("click", funk);
+
+$(document).click(function(e) {
+  if ($(e.target).closest(res).length || $(e.target).closest('.menu-a').length) return;
+  res.fadeOut(100);
+});
+function funk(){
+  $(this).toggleClass("show");
+  var link = $(this).attr('data-class'),
+      el = $('.dropDown-menu.'+link);
+  if(el.css("display") == "none"){
+    res.hide();
+    el.slideToggle(400);
+  }
+  else{
+    el.slideToggle(400);
+  }
+}
+
+$(document).ready(function() { 
+  var overlay = $('#overlay'); 
+  var open_modal = $('.open_modal'); 
+  var close = $('.modal_close, #overlay'); 
+  var modal = $('.modal_div'); 
+  var div;
+
+  open_modal.click(function(event){ 
+      event.preventDefault(); 
+      div = $(this).attr('href'); 
+      overlay.fadeIn(400, 
+          function(){ 
+              $(div) 
+                  .css('display', 'flex') 
+                  .animate({opacity: 1, top: '50%'}, 200); 
+      });
+  });
+
+  close.click( function(){
+    $(div).animate({opacity: 0, top: '265px'}, 200, function () {
+      $(div).css('display', 'none');
+      overlay.fadeOut(400); 
+    });
+  });
+});
+
+
